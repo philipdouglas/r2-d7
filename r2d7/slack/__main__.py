9c58e800c7e39f112872cbf505fa73e4dfc5ed7e
Philip Douglas <psd@pdouglas.net>
2017-08-26 21:04:26 +0100

    Attempt to fix bot

--- r2d7/slack/__main__.py
+++ r2d7/slack/__main__.py
@@ -23,6 +23,39 @@ logger = logging.getLogger(__name__)
 class Droid(SlackDroid, ListFormatter, CardLookup): pass
 
 
+def on_message(ws, message):
+
+    # Access the message type
+    logger.info(message['type'])
+
+    # Access the config defined in the bot.yml (commented avoid error)
+    # print (message['resource']['MY_CUSTOM_CONFIG'])
+
+    pp = pprint.PrettyPrinter(indent=2)
+    pp.pprint(message)
+
+
+# Fires when an error occurred in the connection with the Beep Boop Resourcer server.
+def on_error(ws, error):
+    logger.info ('Error: ' + str(error))
+
+# Fires the connection with the Beep Boop resourcer has closed.
+def on_close(ws):
+    logger.info ('Closed')
+
+# Fires when the connection with the Beep Boop resourcer has opened.
+def on_open(ws):
+    logger.info('Opened')
+
+
+handler_funcs = {
+    'on_open': on_open,
+    'on_message': on_message,
+    'on_error': on_error,
+    'on_close': on_close,
+}
+
+
 def main():
     debug = os.getenv('DEBUG', False)
     log_level = 'DEBUG' if debug else 'INFO'
@@ -39,6 +72,7 @@ def main():
         slack_token = None
         botManager = bot_manager.BotManager(spawn_bot)
         res = resourcer.Resourcer(botManager)
+        res.handlers(handler_funcs)
         res.start()
     else:
         # only want to run a single instance of the bot in dev mode

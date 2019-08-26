e72e6c73cb25c488dde00ad34d068d1a4cd56841
Philip Douglas <psd@pdouglas.net>
2019-08-26 16:53:29 +0100

    Restart bots if they crash

--- r2d7/bots/__main__.py
+++ r2d7/bots/__main__.py
@@ -1,7 +1,27 @@
 from multiprocessing import Process
+import time
+import logging
 
 from r2d7.slack.__main__ import main as slack_main
 from r2d7.discord.__main__ import main as discord_main
+
+logger = logging.getLogger(__name__)
+
+
+def auto_restarter(slack, discord):
+    """
+    If either bot crashes, start it up again. Last line of defence against crashes.
+    """
+    while True:
+        time.sleep(60)
+        if not slack.is_alive():
+            logger.warning("AUTO RESTARTER: Slack bot down, restarting...")
+            slack = Process(target=slack_main)
+            slack.start()
+        if not discord.is_alive():
+            logger.warning("AUTO RESTARTER: Discord bot down, restarting...")
+            discord = Process(target=discord_main)
+            discord.start()
 
 
 def main():
@@ -10,8 +30,7 @@ def main():
     discord = Process(target=discord_main)
     discord.start()
 
-    slack.join()
-    discord.join()
+    auto_restarter(slack, discord)
 
 if __name__ == "__main__":
     main()
